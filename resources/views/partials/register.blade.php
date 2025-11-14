807878cf09b9371751c59e55501f48142ca1050a
Henry Jr Zai <hjuniorzai@gmail.com>
2025-11-14 13:51:21 +0700

    [UPDATE] : menghapus inputan alamat dan membuat notifikasi validasi

--- resources/views/partials/register.blade.php
+++ resources/views/partials/register.blade.php
@@ -5,15 +5,7 @@
             <div class="modal-log-wrap list-file-delete">
                 <h5 class="title fw-semibold">Daftar Akun Customer</h5>
 
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul class="mb-0">
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
+
 
                 @if (session("register_success"))
                     <div class="alert alert-success">
@@ -28,53 +20,71 @@
                             <label class="fw-semibold body-md-2">
                                 Nama Lengkap *
                             </label>
-                            <input type="text" name="nama" placeholder="Masukkan nama lengkap"
+                            <input type="text" name="nama" placeholder="Masukkan nama lengkap" class="@error('nama') is-invalid @enderror"
                                 value="{{ old("nama") }}" required>
+                            @error('nama')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </fieldset>
 
                         <fieldset class="mb-3">
                             <label class="fw-semibold body-md-2">
                                 Username *
                             </label>
-                            <input type="text" name="username" placeholder="Masukkan username"
+                            <input type="text" name="username" placeholder="Masukkan username" class="@error('username') is-invalid @enderror"
                                 value="{{ old("username") }}" required>
+                            @error('username')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </fieldset>
 
                         <fieldset class="mb-3">
                             <label class="fw-semibold body-md-2">
                                 Email *
                             </label>
-                            <input type="email" name="email" placeholder="Masukkan email"
+                            <input type="email" name="email" placeholder="Masukkan email" class="@error('email') is-invalid @enderror"
                                 value="{{ old("email") }}" required>
+                            @error('email')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </fieldset>
 
                         <fieldset class="mb-3">
                             <label class="fw-semibold body-md-2">
                                 No. HP *
                             </label>
-                            <input type="text" name="no_hp" placeholder="Masukkan nomor HP"
+                            <input type="text" name="no_hp" placeholder="Masukkan nomor HP" class="@error('no_hp') is-invalid @enderror"
                                 value="{{ old("no_hp") }}" required>
+                            @error('no_hp')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </fieldset>
 
-                        <fieldset class="mb-3">
+                        {{-- <fieldset class="mb-3">
                             <label class="fw-semibold body-md-2">
                                 Alamat *
                             </label>
                             <textarea name="alamat" placeholder="Masukkan alamat lengkap" rows="3" required>{{ old("alamat") }}</textarea>
-                        </fieldset>
+                        </fieldset> --}}
 
                         <fieldset class="mb-3">
                             <label class="fw-semibold body-md-2">
                                 Password *
                             </label>
-                            <input type="password" name="password" placeholder="Masukkan password" required>
+                            <input type="password" name="password" placeholder="Masukkan password" class="@error('password') is-invalid @enderror" required>
+                            @error('password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </fieldset>
 
                         <fieldset class="mb-3">
                             <label class="fw-semibold body-md-2">
                                 Konfirmasi Password *
                             </label>
-                            <input type="password" name="password_confirmation" placeholder="Ulangi password" required>
+                            <input type="password" name="password_confirmation" placeholder="Ulangi password" class="@error('password_confirmation') is-invalid @enderror" required>
+                            @error('password_confirmation')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </fieldset>
                     </div>
                     <button type="submit" class="tf-btn w-100 text-white">
